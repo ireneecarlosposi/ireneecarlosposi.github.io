35d14e33c3a7fe35f2dc1664e0aecb7dd87edf7c
Marco Manco <marcomanco1998@gmail.com>
2022-07-12 23:45:29 +0200

    feat: add img and story

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -3,9 +3,19 @@ import Grid from "@mui/material/Grid";
 import fiori from "../../img/svg/fiori.svg";
 import fourteen from "../../img/img_story/2014.JPG";
 import fifteen from "../../img/img_story/2015.JPG";
-import { Gif } from "@mui/icons-material";
 
-const rootStyle = { marginTop: "-1em", backgroundColor: "#F3F4F5" };
+const rootStyle = {
+  marginTop: "-1em",
+  //backgroundColor: "#d7ad89" };
+  background:
+    "linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(215,173,137,1) 100%)",
+};
+
+const borderStyle = {
+  borderStyle: "solid",
+  borderWidth: 5,
+  borderColor: "#000000",
+};
 
 function Story() {
   return (
@@ -19,22 +29,27 @@ function Story() {
         style={{ ...rootStyle }}
       >
         <Grid item>
-          <h1>La nostra Storia</h1>
+          <h1 style={{ fontSize: "3em" }}>La nostra Storia</h1>
         </Grid>
         <Grid item>
           <img src={fiori} alt="fiori" />
         </Grid>
         <Grid item>
+          <h1
+            style={{ marginTop: "-10%", marginLeft: "15em", fontSize: "3em" }}
+          >
+            2014 - 2015
+          </h1>
           <img
             src={fourteen}
             alt="2014"
-            width="467"
-            height="350"
+            // width="467"
+            // height="350"
             style={{
+              width: "27em",
+              height: "21em",
               marginLeft: "50%",
-              borderStyle: "solid",
-              borderWidth: 5,
-              borderColor: "#000000",
+              ...borderStyle,
             }}
           />
         </Grid>
@@ -42,14 +57,14 @@ function Story() {
           <img
             src={fifteen}
             alt="2015"
-            width="400"
-            height="400"
+            // width="400"
+            // height="400"
             style={{
+              width: "25em",
+              height: "25em",
               marginTop: "-15em",
               marginRight: "15em",
-              borderStyle: "solid",
-              borderWidth: 5,
-              borderColor: "#000000",
+              ...borderStyle,
             }}
           />
         </Grid>
